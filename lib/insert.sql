a89168eef5c56b768a7c56fba65742548078f6b4
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-07-01 16:04:42 -0700

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ("name", "age", "sex") VALUES ("Mr.Chocolate", 3, "M");
+INSERT INTO bears ("name", "age", "sex") VALUES ("Rowdy", 4, "M");
+INSERT INTO bears ("name", "age", "sex") VALUES ("Tabitha", 6, "F");
+INSERT INTO bears ("name", "age", "sex") VALUES ("Sergeant Brown", 7, "F");
+INSERT INTO bears ("name", "age", "sex") VALUES ("Melissa", 6, "M");
+INSERT INTO bears ("name", "age", "sex") VALUES ("Grinch", 3, "M");
+INSERT INTO bears ("name", "age", "sex") VALUES ("Wendy", 1, "F");
+INSERT INTO bears ("name", "age", "sex") VALUES (NULL, 5, "M");
